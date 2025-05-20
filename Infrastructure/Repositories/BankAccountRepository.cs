b990a850d6f73b463109098bcadc6956cebacf99
serhiy.krasovskyy <xhunter74@gmail.com>
2025-05-20 20:32:29 +0300

    Created base repository. Changed sequences of calls: dispatched events before save.

--- Infrastructure/Repositories/BankAccountRepository.cs
+++ Infrastructure/Repositories/BankAccountRepository.cs
@@ -3,30 +3,20 @@ using DDDExample.Domain.Events;
 
 namespace DDDExample.Infrastructure.Repositories;
 
-public class BankAccountRepository : IBankAccountRepository
+public class BankAccountRepository : BaseRepository, IBankAccountRepository
 {
-    private readonly BankingDbContext _context;
-    private readonly IDomainEventDispatcher _dispatcher;
-
     public BankAccountRepository(BankingDbContext context, IDomainEventDispatcher dispatcher)
+        : base(context, dispatcher)
     {
-        _context = context;
-        _dispatcher = dispatcher;
     }
 
     public async Task<BankAccount?> GetByIdAsync(Guid id)
     {
-        return await _context.BankAccounts.FindAsync(id);
+        return await context.BankAccounts.FindAsync(id);
     }
 
     public async Task AddAsync(BankAccount account)
     {
-        await _context.BankAccounts.AddAsync(account);
-    }
-
-    public async Task SaveChangesAsync()
-    {
-        await _context.SaveChangesAsync();
-        await _context.DispatchDomainEventsAsync(_dispatcher);
+        await context.BankAccounts.AddAsync(account);
     }
 }
---
+++ Infrastructure/Repositories/BaseRepository.cs
@@ -0,0 +1,21 @@
+﻿using DDDExample.Domain.Events;
+
+namespace DDDExample.Infrastructure.Repositories;
+
+public abstract class BaseRepository
+{
+    protected readonly BankingDbContext context;
+    private readonly IDomainEventDispatcher _dispatcher;
+
+    public BaseRepository(BankingDbContext context, IDomainEventDispatcher dispatcher)
+    {
+
+        this.context = context;
+        this._dispatcher = dispatcher;
+    }
+    public async Task SaveChangesAsync()
+    {
+        await context.DispatchDomainEventsAsync(_dispatcher);
+        await context.SaveChangesAsync();
+    }
+}
